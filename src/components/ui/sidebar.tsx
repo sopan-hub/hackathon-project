93968d58c2801251d9b01cca90e3d1ebfb05dd2a
Sopan Patil <patilsopan4148@gmail.com>
2025-09-15 17:51:40 +0000

    now only make the side bar 3d as th card only the side withoutchanging a

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -235,7 +235,7 @@ const Sidebar = React.forwardRef<
         />
         <div
           className={cn(
-            "duration-200 fixed inset-y-0 z-10 hidden h-svh transition-[left,right,width] ease-linear md:flex eco-surface",
+            "duration-200 fixed inset-y-0 z-10 hidden h-svh transition-[left,right,width] ease-linear md:flex eco-sidebar",
             side === "left"
               ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
               : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
